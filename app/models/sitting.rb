341cac249b199bc264dc0c55769c805bd150a4a8
Nilo Ballener <nballener@gmail.com>
2017-09-30 08:13:08 +1000

    add references to person model

--- app/models/sitting.rb
+++ app/models/sitting.rb
@@ -1,2 +1,3 @@
 class Sitting < ApplicationRecord
+  has_many :people
 end
--- db/migrate/20170929220549_create_people.rb
+++ db/migrate/20170929220549_create_people.rb
@@ -4,6 +4,7 @@ class CreatePeople < ActiveRecord::Migration[5.1]
       t.string :name
       t.decimal :order_amount
       t.decimal :paid_amount
+      t.references :sitting
 
       t.timestamps
     end
