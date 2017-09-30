e37c46f11e40d325472556295840a312ca6439fc
Nilo Ballener <nballener@gmail.com>
2017-09-30 17:25:25 +1000

    add owning to model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,11 @@
 class Person < ApplicationRecord
   belongs_to :sitting
 
+  def calculate_owing
+    owing = self.order_amount - self.paid_amount
+    owing > 0 ? owing : 0
+  end
+
   def calculate_change
     change = self.paid_amount - self.order_amount
     change > 0 ? change : 0
